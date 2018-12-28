ce5ff7fd448a81d07aebb3d356cf02ec559d173b
Troels Schwarz-Linnet <tlinnet@gmail.com>
2018-12-28 13:56:29 +0100

    Adding info to run command

--- notebooks_run.cmd
+++ notebooks_run.cmd
@@ -1,2 +1,4 @@
 docker run --rm -p 8888:8888 -v "%CD%":/home/jovyan/work --name csharp tlinnet/csharp-notebook:notebooks
+echo "Copy the secret token from the cmd window"
+echo "Visit http://localhost:8888"
 cmd /k
